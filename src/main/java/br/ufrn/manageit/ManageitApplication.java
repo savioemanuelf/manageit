5028a8e9d6fb619e2cfbc488bb7a4db49ab953c0
Sávio Emanuel <130401186+savioemanuelf@users.noreply.github.com>
2026-01-28 21:15:07 -0300

    chore: bootstrap project

---
+++ src/main/java/br/ufrn/manageit/ManageitApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.manageit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/manageit/ManageitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.manageit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
