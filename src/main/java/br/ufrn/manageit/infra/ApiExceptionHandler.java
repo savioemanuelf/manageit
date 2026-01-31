cec6cc3ed22189937f0688431cdfb231bdb64507
Sávio Emanuel <130401186+savioemanuelf@users.noreply.github.com>
2026-01-31 16:47:57 -0300

    refactor: add handler for RegraNegocioException

--- src/main/java/br/ufrn/manageit/infra/ApiExceptionHandler.java
+++ src/main/java/br/ufrn/manageit/infra/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package br.ufrn.manageit.infra;
 
 import br.ufrn.manageit.infra.exception.BuscaInvalidaException;
 import br.ufrn.manageit.infra.exception.RecursoNaoEncontradoException;
+import br.ufrn.manageit.infra.exception.RegraNegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +10,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(RegraNegocioException.class)
+    public RestErrorMessage handleBusinessViolation(RegraNegocioException ex) {
+        return new RestErrorMessage(
+                HttpStatus.UNPROCESSABLE_CONTENT.value(),
+                ex.getMessage()
+        );
+    }
+
     @ExceptionHandler(RecursoNaoEncontradoException.class)
     public RestErrorMessage handleNotFound(RecursoNaoEncontradoException ex) {
         return new RestErrorMessage(
@@ -19,7 +28,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(BuscaInvalidaException.class)
-    public RestErrorMessage handleNotFound(BuscaInvalidaException ex) {
+    public RestErrorMessage handleBadRequest(BuscaInvalidaException ex) {
         return new RestErrorMessage(
                 HttpStatus.BAD_REQUEST.value(),
                 "A busca não pôde ser concluída. " + ex.getMessage()
