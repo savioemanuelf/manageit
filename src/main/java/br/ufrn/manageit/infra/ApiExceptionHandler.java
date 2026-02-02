62cf6c8954ce18cb3312ac64eeee6a212ad34766
Sávio Emanuel <130401186+savioemanuelf@users.noreply.github.com>
2026-02-02 16:08:23 -0300

    refactor: wrap custom exception handlers with ResponseEntity

--- src/main/java/br/ufrn/manageit/infra/ApiExceptionHandler.java
+++ src/main/java/br/ufrn/manageit/infra/ApiExceptionHandler.java
@@ -4,6 +4,8 @@ import br.ufrn.manageit.infra.exception.BuscaInvalidaException;
 import br.ufrn.manageit.infra.exception.RecursoNaoEncontradoException;
 import br.ufrn.manageit.infra.exception.RegraNegocioException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -11,27 +13,37 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(RegraNegocioException.class)
-    public RestErrorMessage handleBusinessViolation(RegraNegocioException ex) {
-        return new RestErrorMessage(
-                HttpStatus.UNPROCESSABLE_CONTENT.value(),
-                ex.getMessage()
-        );
+    public ResponseEntity<RestErrorMessage> handleBusinessViolation(RegraNegocioException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_CONTENT.value())
+                .body(new RestErrorMessage
+                        (HttpStatus.UNPROCESSABLE_CONTENT.value(),
+                        ex.getMessage()
+                        )
+                );
     }
 
     @ExceptionHandler(RecursoNaoEncontradoException.class)
-    public RestErrorMessage handleNotFound(RecursoNaoEncontradoException ex) {
-        return new RestErrorMessage(
-                HttpStatus.NOT_FOUND.value(),
-                "Recurso não encontrado. Verifique se digitou as " +
-                        "informações corretamente. " + ex.getMessage()
+    public ResponseEntity<RestErrorMessage> handleNotFound(RecursoNaoEncontradoException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND.value())
+                .body(new RestErrorMessage(
+                    HttpStatus.NOT_FOUND.value(),
+                    "Recurso não encontrado. Verifique se digitou as " +
+                    "informações corretamente. " + ex.getMessage()
+
+                )
         );
     }
 
     @ExceptionHandler(BuscaInvalidaException.class)
-    public RestErrorMessage handleBadRequest(BuscaInvalidaException ex) {
-        return new RestErrorMessage(
+    public ResponseEntity<RestErrorMessage> handleBadRequest(BuscaInvalidaException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(new RestErrorMessage(
                 HttpStatus.BAD_REQUEST.value(),
                 "A busca não pôde ser concluída. " + ex.getMessage()
+                )
         );
     }
 
